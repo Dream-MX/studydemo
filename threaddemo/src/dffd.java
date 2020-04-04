47be68f5dcd65bdefb85670d9af8ea46ef7440f4
雷祥 <530436057@qq.com>
2020-04-04 16:36:23 +0800

    dfg

--- threaddemo/src/dffd.java
+++ threaddemo/src/dffd.java
@@ -9,7 +9,7 @@ import java.sql.SQLOutput;
  */
 public class dffd {
     public static void main(String[] args) {
-        System.out.println("rr");
-        System.out.println("aaa");
+        System.out.println("rrddddddddd");
+        System.out.println("aaaffffffffffffffff");
     }
 }
