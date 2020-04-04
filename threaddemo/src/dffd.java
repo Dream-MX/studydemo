d41664d49280a80cbe67f21c7ebad08187b0d950
雷祥 <530436057@qq.com>
2020-04-04 16:24:03 +0800

    l

--- threaddemo/src/dffd.java
+++ threaddemo/src/dffd.java
@@ -9,7 +9,7 @@ import java.sql.SQLOutput;
  */
 public class dffd {
     public static void main(String[] args) {
-        System.out.println("rrrrrrrrrrr");
-        System.out.println("aaaaaaaaaaaaagggggggggggggggg");
+        System.out.println("rr");
+        System.out.println("aaa");
     }
 }
